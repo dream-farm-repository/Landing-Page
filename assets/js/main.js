aca145f3ac0f04e45abd971c756789a46bd867d8
khusniridh0 <pribad9983@gmail.com>
2024-06-11 17:03:24 +0700

    finising langing page responsive, aos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 const header = document.querySelector('.header')
+const animate = document.querySelectorAll('.animate')
 
 function activeNav() {
 	if (window.scrollY > 100) {
@@ -10,8 +11,25 @@ function activeNav() {
 
 window.addEventListener('scroll', () => {
 	activeNav()
+
+	// console.log(window.scrollY);
+
+
+	// animate.forEach((item) => {
+	// 	if (item.offsetTop > window.scrollY) {
+	// 		item.classList.add('animate__animated')
+	// 		console.log(item);
+	// 	}
+	// })
+
+
 })
 
 window.addEventListener('load', () => {
 	activeNav()
+
+
+	AOS.init()
+
+	document.documentElement.style.setProperty('--animate-duration', '0.5s');
 })
